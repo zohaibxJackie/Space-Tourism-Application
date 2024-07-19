dc73f07e61dbe7bfffaff0d6adda76405eda9c46
zohaibxJackie <zkhan52607@gmail.com>
2024-07-19 12:23:03 +0500

    fixed deployment bugs

--- src/App.js
+++ src/App.js
@@ -3,53 +3,49 @@ import { useEffect, useState } from "react";
 import './style.css';
 import './utils.css';
 
-
-import Header from './components/header'
+import Header from './components/header';
 import Home from './components/Home';
 import Destination from './components/Destination';
 import Crew from './components/Crew';
 import Technology from './components/Technology';
-// these are the images for Desktop
-import homeBg from './assets/home/background-home-desktop.jpg'
-import destinationBg from './assets/destination/background-destination-desktop.jpg'
-import crewBg from './assets/crew/background-crew-desktop.jpg'
-import technologyBg from './assets/technology/background-technology-desktop.jpg'
-// These are the image for tablet devices
-import homeBgTablet from './assets/home/background-home-tablet.jpg'
-import destinationBgTablet from './assets/destination/background-destination-tablet.jpg'
-import crewBgTablet from './assets/crew/background-crew-tablet.jpg'
-import technologyBgTablet from './assets/technology/background-technology-tablet.jpg'
-// These are the image for Mobile devices
-import homeBgMobile from './assets/home/background-home-mobile.jpg'
-import destinationBgMobile from './assets/destination/background-destination-mobile.jpg'
-import crewBgMobile from './assets/crew/background-crew-mobile.jpg'
-import technologyBgMobile from './assets/technology/background-technology-mobile.jpg'
 
+import homeBg from './assets/home/background-home-desktop.jpg';
+import destinationBg from './assets/destination/background-destination-desktop.jpg';
+import crewBg from './assets/crew/background-crew-desktop.jpg';
+import technologyBg from './assets/technology/background-technology-desktop.jpg';
+import homeBgTablet from './assets/home/background-home-tablet.jpg';
+import destinationBgTablet from './assets/destination/background-destination-tablet.jpg';
+import crewBgTablet from './assets/crew/background-crew-tablet.jpg';
+import technologyBgTablet from './assets/technology/background-technology-tablet.jpg';
+import homeBgMobile from './assets/home/background-home-mobile.jpg';
+import destinationBgMobile from './assets/destination/background-destination-mobile.jpg';
+import crewBgMobile from './assets/crew/background-crew-mobile.jpg';
+import technologyBgMobile from './assets/technology/background-technology-mobile.jpg';
 
 const bgMap = {
-  "/": { desktop: homeBg, tablet: homeBgTablet, mobile: homeBgMobile },
-  "/destination": { desktop: destinationBg, tablet: destinationBgTablet, mobile: destinationBgMobile },
-  "/crew": { desktop: crewBg, tablet: crewBgTablet, mobile: crewBgMobile },
-  "/technology": { desktop: technologyBg, tablet: technologyBgTablet, mobile: technologyBgMobile },
+  "/Space-Tourism-Application/": { desktop: homeBg, tablet: homeBgTablet, mobile: homeBgMobile },
+  "/Space-Tourism-Application/destination": { desktop: destinationBg, tablet: destinationBgTablet, mobile: destinationBgMobile },
+  "/Space-Tourism-Application/crew": { desktop: crewBg, tablet: crewBgTablet, mobile: crewBgMobile },
+  "/Space-Tourism-Application/technology": { desktop: technologyBg, tablet: technologyBgTablet, mobile: technologyBgMobile },
 };
 
 function App() {
   const location = useLocation();
-  const [backgroundImg, setBackground] = useState(bgMap["/"].desktop);
-  const [choose, setChoose] = useState({num: "", txt: ""});
+  const [backgroundImg, setBackground] = useState(bgMap["/Space-Tourism-Application/"].desktop);
+  const [choose, setChoose] = useState({ num: "", txt: "" });
 
-  function chooseSomething() {
-    if (window.location.pathname === "/") {
-      setChoose({num: "", txt: ""});
-    } else if (window.location.pathname === "/destination") {
-      setChoose({num: "01", txt: "pick your destination"});
-    } else if (window.location.pathname === "/crew") {
-      setChoose({num: "02", txt: "meet your crew"});
-    } else {
-      setChoose({num: "03", txt: "space launch 101"});
+  const chooseSomething = () => {
+    const path = location.pathname;
+    if (path === `${process.env.PUBLIC_URL}/` || path === '/Space-Tourism-Application/') {
+      setChoose({ num: "", txt: "" });
+    } else if (path === `${process.env.PUBLIC_URL}/destination` || path === '/Space-Tourism-Application/destination') {
+      setChoose({ num: "01", txt: "pick your destination" });
+    } else if (path === `${process.env.PUBLIC_URL}/crew` || path === '/Space-Tourism-Application/crew') {
+      setChoose({ num: "02", txt: "meet your crew" });
+    } else if (path === `${process.env.PUBLIC_URL}/technology` || path === '/Space-Tourism-Application/technology') {
+      setChoose({ num: "03", txt: "space launch 101" });
     }
-  }
-
+  };
 
   useEffect(() => {
     const updateBackground = () => {
@@ -60,7 +56,8 @@ function App() {
       } else if (width <= 1024) {
         device = 'tablet';
       }
-      setBackground(bgMap[location.pathname][device] || bgMap["/"].desktop);
+      const path = location.pathname.replace(process.env.PUBLIC_URL, '');
+      setBackground(bgMap[path]?.[device] || bgMap["/Space-Tourism-Application/"].desktop);
     };
     updateBackground();
     chooseSomething();
@@ -71,19 +68,19 @@ function App() {
     };
   }, [location.pathname]);
 
-
-
   return (
     <div className='main' style={{ backgroundImage: `url(${backgroundImg})` }}>
       <Header />
       <div className="choose-destination-text">
-        <p className="uppercase font-berlow-condensed fs-letter-spacing"><span aria-hidden="true" className="opacity-low">{choose.num}</span>{choose.txt}</p>
+        <p className="uppercase font-berlow-condensed fs-letter-spacing">
+          <span aria-hidden="true" className="opacity-low">{choose.num}</span>{choose.txt}
+        </p>
       </div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Destination" element={<Destination />} />
-        <Route path="/Crew" element={<Crew />} />
-        <Route path="/Technology" element={<Technology />} />
+        <Route path={`/`} element={<Home />} />
+        <Route path={`/destination`} element={<Destination />} />
+        <Route path={`/crew`} element={<Crew />} />
+        <Route path={`/technology`} element={<Technology />} />
       </Routes>
     </div>
   );
